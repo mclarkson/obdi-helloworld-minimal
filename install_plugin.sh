ccc7ced7c90476c9616de066aa0b17fcc1d2fed3
Mark Clarkson <mark.clarkson@smorg.co.uk>
2016-03-24 14:37:23 +0000

    first commit

---
+++ install_plugin.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#
+# Obdi - a REST interface and GUI for deploying software
+# Copyright (C) 2014  Mark Clarkson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# saltconfigserver plugin
+
+[[ -z $guid ]] && {
+  echo "ERROR: environment variable 'guid' must be set."
+  exit 1
+}
+
+##
+## TODO: This plugin depends on salt
+##
+
+proto="https"
+opts="-k -s" # don't check ssl cert, silent
+ipport="127.0.0.1:443"
+
+#
+# Create a temporary file and a trap to delete it
+#
+
+t="/tmp/install_helloworld_$$"
+touch $t
+[[ $? -ne 0 ]] && {
+    echo "Could not create temporary file. Aborting."
+    exit 1
+}
+trap "rm -f -- '$t'" EXIT
+
+#
+# Create the plugin entry in obdi, so it can be shown in the sidebar
+#
+
+curl -k -d '{
+    "Name":"helloworld",
+    "Desc":"Hello World plugin.",
+    "HasView":1,
+    "Parent":""
+}' $proto://$ipport/api/admin/$guid/plugins | tee $t
+
+# Grab the id of the last insert
+id=`grep Id $t | grep -Eo "[0-9]+"`
+
+#
+# Add the AJS controller files
+#
+# These need to be loaded when the application starts
+#
+
+curl -k -d '{
+    "Name":"helloworld.js",
+    "Desc":"Controller for HelloWorld.",
+    "Type":1,
+    "PluginId":'"$id"',
+    "Url":"helloworld/js/controllers/helloworld.js"
+}' $proto://$ipport/api/admin/$guid/files
+
+# Delete the temporary file and delete the trap
+rm -f -- "$t"
+trap - EXIT
+
---
+++ js/controllers/helloworld.js
@@ -0,0 +1,202 @@
+// Obdi - a REST interface and GUI for deploying software
+// Copyright (C) 2014  Mark Clarkson
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// ------------------------------------------------------------------------
+// AngularJS Controller
+// ------------------------------------------------------------------------
+
+mgrApp.controller("helloWorld", function ($scope,$http,$uibModal,$log,
+      $timeout,baseUrl,$rootScope) {
+
+  $scope.environments = [];
+  $scope.env = {};
+
+  // Pages
+  $scope.mainview = true;
+
+  // Alerting
+  $scope.message = "";
+  $scope.mainmessage = "";
+  $scope.okmessage = "";
+  $scope.login.error = false;
+
+  // Hiding/Showing
+  $scope.showkeybtnblockhidden = false;
+  $scope.btnsayhellodisabled = true;
+  $scope.btnsayhellopressed = false;
+  $scope.btnenvlistdisabled = false;
+  $scope.envchosen = false;
+  $scope.status = {};
+
+  $rootScope.$broadcast( "searchdisabled", true );
+
+  // ----------------------------------------------------------------------
+  $scope.$on( "search", function( event, args ) {
+  // ----------------------------------------------------------------------
+    if( $scope.grainsview.show == false ) {
+      $scope.hostfilter = args;
+      $scope.checkbox_allnone = false;
+      for( var i=0; i < $scope.servernames.length; i=i+1 ) {
+        $scope.servernames[i].Selected = false;
+      }
+      ReviewBtnStatus();
+    } else {
+      $scope.grainfilter = args;
+    }
+  });
+
+  // ----------------------------------------------------------------------
+  var clearMessages = function() {
+  // ----------------------------------------------------------------------
+    $scope.message = "";
+    $scope.mainmessage = "";
+    $scope.okmessage = "";
+    $scope.login.error = false;
+    $scope.error = false;
+  }
+
+  // ----------------------------------------------------------------------
+  $scope.Restart = function() {
+  // ----------------------------------------------------------------------
+    clearMessages();
+    $scope.btnlistserversdisabled = true;
+    $scope.btnenvlistdisabled = false;
+    $scope.btnapplydisabled = true;
+    $scope.btnreviewdisabled = true;
+    $scope.listbtnpressed = false;
+    $scope.serverlist_ready = false;
+    $scope.serverlist_empty = false;
+    $scope.envchosen = false;
+    $scope.reviewpage.enabled = false;
+    $scope.applypage.enabled = false;
+    $scope.applypage.complete = false;
+    $scope.resultlist_empty = false;
+    $scope.resultlist_ready = false;
+  };
+
+  // ----------------------------------------------------------------------
+  $scope.envChoice = function( envobj, $event ) {
+  // ----------------------------------------------------------------------
+    clearMessages();
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.envchosen = true;
+    $scope.btnlistserversdisabled = false;
+    $scope.btnenvlistdisabled = true;
+    $scope.env = envobj;
+    $scope.btnsayhellodisabled = false;
+    $scope.status.isopen = !$scope.status.isopen;
+  };
+
+  // ----------------------------------------------------------------------
+  $scope.GoBack = function( ) {
+  // ----------------------------------------------------------------------
+    clearMessages();
+    $scope.mainview = true;
+    $scope.btnenvlistdisabled = true;
+    $scope.btnsayhellodisabled = false;
+
+    $rootScope.$broadcast( "setsearchtext", $scope.hostfilter );
+  }
+
+  // ----------------------------------------------------------------------
+  $scope.FillEnvironmentsTable = function() {
+  // ----------------------------------------------------------------------
+
+    $http({
+      method: 'GET',
+      url: baseUrl + "/" + $scope.login.userid + "/" + $scope.login.guid
+           + "/envs?writeable=1"
+           + '&time='+new Date().getTime().toString()
+    }).success( function(data, status, headers, config) {
+
+      $scope.environments = data;
+      if( data.length == 0 ) {
+        $scope.serverlist_empty = true;
+        $scope.btnenvlistdisabled = true;
+      }
+
+    }).error( function(data,status) {
+      if (status>=500) {
+        $scope.login.errtext = "Server error.";
+        $scope.login.error = true;
+        $scope.login.pageurl = "login.html";
+      } else if (status==401) {
+        $scope.login.errtext = "Session expired.";
+        $scope.login.error = true;
+        $scope.login.pageurl = "login.html";
+      } else if (status>=400) {
+        clearMessages();
+        $scope.mainmessage = "Server said: " + data['Error'];
+      } else if (status==0) {
+        // This is a guess really
+        $scope.login.errtext = "Could not connect to server.";
+        $scope.login.error = true;
+        $scope.login.pageurl = "login.html";
+      } else {
+        $scope.login.errtext = "Logged out due to an unknown error.";
+        $scope.login.error = true;
+        $scope.login.pageurl = "login.html";
+      }
+    });
+  };
+
+  $scope.FillEnvironmentsTable();
+
+  // Modal dialog
+
+  // --------------------------------------------------------------------
+  $scope.dialog = function (name) {
+  // --------------------------------------------------------------------
+
+    $scope.classname = classname;
+
+    var modalInstance = $uibModal.open({
+      templateUrl: 'myModalContent.html',
+      controller: $scope.ModalInstanceCtrl,
+      size: 'sm',
+      resolve: {
+        // the classname variable is passed to the ModalInstanceCtrl
+        classname: function () {
+          return $scope.classname;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (classname) {
+      $log.info('Will delete: ' + $scope.classname );
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  // --------------------------------------------------------------------
+  $scope.ModalInstanceCtrl = function ($scope, $uibModalInstance, classname) {
+  // --------------------------------------------------------------------
+
+    // So the template can access 'classname' in this new scope
+    $scope.classname = classname;
+
+    $scope.ok = function () {
+      $uibModalInstance.close();
+    };
+
+    $scope.cancel = function () {
+      $uibModalInstance.dismiss('cancel');
+    };
+  };
+
+});
